03964346276f50f4ea5fec03a6e7dc13a438242e
José O. Sotero Esteva <jose.sotero@upr.edu>
2020-03-20 08:36:30 -0400

    Los ciclos se estaban quedando con 5 vértices. En DFS remoción del último vertice del ciclo movido después de su almacenamiento en LISTA.
    Con el arreglo anterior se ve ue hay pares de ciclos del mismo hexágono en direcciones contrarias. Ejemplo [571, 587, 803, 619, 814, 588] y [571, 588, 814, 619, 803, 587].
    
    Robarse la variable en DFS y en countCycles es inecesario cuando la misma información está en graph.
    
    Hay problemas con el grafo. Ejemplo: el nodo 804 tiene nodos adyacentes 810 y 814 pero graph.nodes() no tiene esos dos nodos.
    
    Se usa set y frozenset para descartar ciclos duplicados. LISTA es ahora cycles_as_sets.
    
    Cambia campo center de Hexagon por método center() para evitar ambiguedad.

--- hex2point.py
+++ hex2point.py
@@ -4,11 +4,11 @@ import granules.structure.NAMDdata as gr
 # Python Program to count 
 # cycles of length n 
 # in a given graph. 
-class hexagon:
+class Hexagon:
     def __init__(self,vert,coords):
         self.vertices = tuple(vert.tolist())
         self.coordinates = coords
-        self.center = tuple(coords.mean(axis=0).tolist())
+        #self.center = tuple(coords.mean(axis=0).tolist()) # redundancia peligrosa
         
     def __str__(self):
         #permits printing
@@ -16,7 +16,7 @@ class hexagon:
     
     def isneighbor(self, other): #adyacencia
         #check if has exacly 2 atoms in common with other hexagon
-        return len(set(self.vertices).intersection(other.vertices)) == 2
+        return len(frozenset(self.vertices).intersection(other.vertices)) == 2
     
     def __eq__(self,other):
         #permits use of ==
@@ -28,51 +28,47 @@ class hexagon:
     
     def __hash__(self):
         #para crear el grafo
-        return hash((self.vertices,self.center))
+        return hash((self.vertices,self.center()))
     
     def __repr__(self):
         #mejor representación al imprimir edges del grafo
-        t = tuple(f"{i:.2f}" for i in self.center)
+        t = tuple(f"{i:.2f}" for i in self.center())
         return f"hexagon{t}"
     
+    def center(self):
+        return tuple(self.coordinates.mean(axis=0).tolist())
 
 #from https://www.geeksforgeeks.org/cycles-of-length-n-in-an-undirected-and-connected-graph/
 # Number of vertices 
 #V=6
-#lista para aguantar los ciclos y las marcas
-LISTA = []
 
 def DFS(graph, marked, n, vert, start, count, cycle): 
-    global LISTA
-	# mark the vertex vert as visited 
-    marked[vert] = True
+    global cycles_sets
+	
+    marked[vert] = True  # mark the vertex vert as visited 
     cycle.append(vert)
     
-    if cycle : # si cycle tiene algo
-        LISTA.append(list(cycle))
-	# if the path of length (n-1) is found 
-    if n == 0: 
-		# mark vert as un-visited to make it usable again. 
+    if n == 0:  # if the path of length (n-1) is found 
+        cycle_as_set = frozenset(cycle)
+        if graph.has_edge(vert,start): # Check if vertex vert can end with vertex start 
+            count = count + 1
+            cycles_sets.add(cycle_as_set)
+            #print(cycle)
+            #cycles_sets.append(True) #marcar break-points
+            #return count # fin con mismo return en if y else => va afuera
+        #else: 
+            #return count  # fin con mismo return en if y else => va afuera
+
+        # mark vert as un-visited to make it usable again. 
         marked[vert] = False
         cycle.pop()
 
-		# Check if vertex vert can end with vertex start 
-        if graph.has_edge(vert,start): 
-            count = count + 1
-            #print(cycle)
-            #print('ok')
-            LISTA.append(True) #marcar break-points
-            return count 
-        else: 
-            return count 
+        return count 
 
-	# For searching every possible path of 
-	# length (n-1) 
-    for i in range(V): 
-        if marked[i] == False and graph.has_edge(vert,i): 
-
-			# DFS for searching path by decreasing 
-			# length by 1 
+	# For searching every possible path of length (n-1) 
+    for i in graph.nodes(): 
+        if not marked[i] and graph.has_edge(vert,i): 
+            # DFS for searching path by decreasing length by 1 
             count = DFS(graph, marked, n-1, i, start, count,cycle) 
 
 	# marking vert as unvisited to make it 
@@ -88,19 +84,20 @@ def DFS(graph, marked, n, vert, start, count, cycle):
 # N in an undirected 
 # and connected graph. 
 def countCycles( graph, n): 
-	# all vertex are marked un-visited initially. 
-	marked = [False] * len(graph)
+    # all vertex are marked un-visited initially. 
+    marked = [False] * (len(graph) + 1)
 
-	# Searching for cycle by using v-n+1 vertices 
-	count = 0
-	for i in range(V-(n-1)): 
-		count = DFS(graph, marked, n-1, i, i, count, []) 
-
-		# ith vertex is marked as visited and 
-		# will not be visited again. 
-		marked[i] = True
+    # Searching for cycle by using v-n+1 vertices s
+    count = 0
+    for atom in graph.nodes():
+        #print("start: " , atom, " con adyacencias ", graph.neighbors(n))
+        count = DFS(graph, marked, n-1, atom, atom, count, []) 
+        
+        # ith vertex is marked as visited and 
+        # will not be visited again. 
+        #marked[atom] = True
 	
-	return int(count/2) 
+    return int(count/2) 
 
 
 
@@ -117,23 +114,12 @@ V = psf.atoms.shape[0]
 enlaces = zip(psf.bonds['atom1'],psf.bonds['atom2'])
 g = nx.Graph(enlaces)
 
-
 cycle_length = 6
+cycles_sets = set()  #cycles_sets para aguantar los ciclos
+
+
 print("Total cycles of length ",cycle_length," are ",countCycles(g, cycle_length)) #397 hrexagonos
-
-
-
-
-rawlist = []
-# this code is contributed by Shivani Ghughtyal 
-for x in range(len(LISTA)): 
-    if LISTA[x] == True: rawlist.append(frozenset(LISTA[x-1]))
-
-S = set(rawlist)
-print("---------")
-FS = set([frozenset(e) for e in g.edges()])
-print("-----------------")
-
+print("Total unique cycles of length ",cycle_length," are ",len(cycles_sets)) #397 hrexagonos
 
 
 #escoger columnas que nos interesan para hexagonos
@@ -142,16 +128,15 @@ cnt.pdb = cnt.pdb[['ID','x','y','z']]
 
 
 hexagons = []
-for verts in S:
-    table = cnt.pdb[cnt.pdb['ID'].isin(verts)]
-    #vertices
-    v = table['ID'].values
-    #coordenadas
-    c = table[['x','y','z']].values 
+for verts in cycles_sets:
+    hexagon_coordinates = cnt.pdb[cnt.pdb['ID'].isin(verts)]
     
-    h = hexagon(v,c)
+    v = hexagon_coordinates['ID'].values            #vertices
+    
+    c = hexagon_coordinates[['x','y','z']].values   #coordenadas
+    
+    h = Hexagon(v,c)
     hexagons.append(h)
-    #print(h)
 
 
 
@@ -161,10 +146,10 @@ for h1 in hexagons:
         if h1.isneighbor(h2):
             hex_connections.append((h1,h2))
 
+print(hex_connections)
 cgmol = nx.Graph(hex_connections)
-print(cgmol.edges())
+#print(cgmol.edges())
 
 nx.draw(cgmol, with_labels=False, font_weight='bold')
 print("total connections:")
 print(len(cgmol.edges()))
-#1165 connections
