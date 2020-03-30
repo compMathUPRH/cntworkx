121ca45abcaa2dc1dad40c68927de2813dcb684e
Berna <Bernardo-A-Roque>
2020-03-30 01:21:27 -0400

    Made rotation work

--- rotationmatrix.py
+++ rotationmatrix.py
@@ -1,6 +1,16 @@
-import granules.structure.NAMDdata as nd
 import granules.structure.LAMMPSdata as ld
-import pandas as pd
+import numpy as np
+
+def yrotmatrix(trio):
+    np.array([[0,0,1],[0,1,0],[0,0,1]])
 
 # para leer cyclos y coordenadas
 cnt = ld.LammpsData("cgtube.data")
+offset = cnt.atomproperty.atoms[['x','y','z']].mean()
+cnt.atomproperty.atoms[['x','y','z']] = cnt.atomproperty.atoms[['x','y','z']] - offset
+
+for i,row in cnt.atomproperty.atoms[['x','y','z']].iterrows():
+    cnt.atomproperty.atoms.loc[i,['x','y','z']] = row.values.T.dot(np.array([[0,0,1],[0,1,0],[-1,0,0]]))
+    
+cnt.atomproperty.atoms[['x','y','z']] = cnt.atomproperty.atoms[['x','y','z']] + offset
+cnt.writeConf('rotcgtube.data')
